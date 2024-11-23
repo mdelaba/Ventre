9a67a56256818c830e53772204548b9dbfc5fe30
mdelaba <mdelaba@uwo.ca>
2024-11-23 15:45:48 -0400

    Initial commit including subdirectories

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ventre
+Project that connects customers with a 3D printable design to individuals with a 3D printer and facilitates the payment, and delivery of the services between entities.
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const next = require("next");
+const cors = require("cors");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Use the CORS middleware
+  server.use(
+    cors({
+      origin: "http://localhost:5173", // Replace with your frontend's origin
+    })
+  );
+
+  // Define your API routes
+  server.get("/api/payment_intent", (req, res) => {
+    res.json({ data: "Your data here" });
+  });
+
+  // Handle all other requests with Next.js
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  const port = 3000;
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
